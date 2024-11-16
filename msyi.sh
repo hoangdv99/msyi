ca3420b568c8d6e9e17337167adadf883ba661aa
hoangdv99 <hoangdv257@gmail.com>
2024-11-16 21:46:46 +0900

    :tada: Add msyi.sh

---
+++ msyi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "
+🎉 :tada:                Initial commit
+🔖 :bookmark:            Version tag 
+✨ :sparkles:            New feature
+🐛 :bug:                 Bug fix
+🗃️  :card_file_box:       Perform database related changes
+📚 :books:               Documentation
+🐎 :racehorse:           Performance
+🚨 :rotating_light:      Tests
+✅ :white_check_mark:    Adding a test
+✔️  :heavy_check_mark:    Make a test pass
+⚡ :zap:                 General update
+🎨 :art:                 Improve format/structure
+🔨 :hammer:              Refactor code
+🔥 :fire:                Removing codes/files
+⬆️  :arrow_up:            Upgrading deps
+⬇️  :arrow_down:          Downgrading deps
+👕 :shirt:               Lint
+📝 :pencil:              Text
+🚑 :ambulance:           Hot fix
+🚧 :construction:        WIP
+➖ :heavy_minus_sign:    Removing deps
+➕ :heavy_plus_sign:     Adding deps
+🐳 :whale:               Docker
+🔧 :wrench:              Config files
+📦 :package:             Packaging
+💩 :hankey:              Bad code/Need improve
+⏪ :rewind:              Reverting changes
+🚚 :truck:               Moving/Renaming resource
+🙈 :see_no_evil:         Add or update a .gitignore file
+👷 :construction_worker: Adding CI build system
+"
